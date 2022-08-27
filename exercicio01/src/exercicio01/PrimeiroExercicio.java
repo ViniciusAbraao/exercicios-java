6652cb17ef87c9fbc8e5b6dfd10c8df30c86c191
ViniciusAbraao <viniciusabraaocarvalho@gmail.com>
2022-08-27 20:21:30 -0300

    commit inicial

---
+++ exercicio01/src/exercicio01/PrimeiroExercicio.java
@@ -0,0 +1,22 @@
+package exercicio01;
+
+
+public class PrimeiroExercicio {
+	public static void main(String[] args) {
+		int valor1 = 8;
+		int valor2 = 9;
+		int valor3 = 7;
+		
+		int valor4 = 4;
+		int valor5 = 5;
+		int valor6 = 6;
+		
+		int media2 = (valor4+valor5+valor6)/3;
+		int media1 = (valor1+valor2+valor3)/3;
+		
+		int mediafinal = (media1+media2)/2;
+		
+		System.out.println(mediafinal);		
+		
+	}
+}
